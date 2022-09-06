0db492595cfe319b249ac3c4b229cc1f0f7b8cf4
Brs545 <baris.candogan@hotmail.com>
2022-09-06 19:02:06 +0300

    Icons

---
+++ lib/src/Icons/icons.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const iconSvg = "assets/cil_list-filter.svg";
+const vectorSvg = "assets/Vector.svg";
+const userSvg = "assets/ant-design_user-outlined.svg";
+const bazaarSvg = "assets/akar-icons_shopping-bag.svg";
